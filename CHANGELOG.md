3a3e643e4b5cb925800769391b16cbdffe4778e4
Teemu Matilainen <teemu.matilainen@iki.fi>
2017-09-25 22:21:45 +0300

    Release v1.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 1.0.0 / _Not released yet_
+## 1.0.0 / 2017-09-25
 
 - Initial public release
--- lib/yle_tf-landscape/version.rb
+++ lib/yle_tf-landscape/version.rb
@@ -1,5 +1,5 @@
 module YleTfPlugins
   module Landscape
-    VERSION = '1.0.0.dev'.freeze
+    VERSION = '1.0.0'.freeze
   end
 end
